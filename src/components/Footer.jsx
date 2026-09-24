595e9e4941aaf65ce2a3939774a76e32f13259fe
chairulanam1605 <limit.eren1605@gmail.com>
2026-09-24 12:07:41 +0700

    edit text in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="py-12 px-6 bg-slate-900 text-center">
-      <p className="text-[10px] text-slate-500 uppercase tracking-widest italic mb-2">
+      <p className="text-[10px] text-stone-100 uppercase tracking-widest italic mb-2">
         © 2026 SHOWROOM JAYA SENTOSA. Seluruh Hak Cipta Dilindungi
       </p>
-      <p className="text-[10px] text-slate-600 uppercase tracking-widest leading-relaxed">
+      <p className="text-[10px] text-stone-100 uppercase tracking-widest leading-relaxed">
         Jln. Gombong-Klampok Pala RT 03 RW 06 Jurang - Jero, Desa Sampang, Kec. Sempor, Kab. Kebumen 
       </p>
     </footer>
